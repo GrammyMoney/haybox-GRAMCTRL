71def811cac9d95c88a079c75f94adb00c789856
7SlicesOfBread <breadbandit7@gmail.com>
2024-11-01 17:02:41 -0700

    Update Rivals2.cpp
    Added WASD support

--- src/modes/Rivals2.cpp
+++ src/modes/Rivals2.cpp
@@ -53,7 +53,7 @@ void Rivals2::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
         inputs.left,
         inputs.right,
         inputs.down,
-        inputs.up,
+        inputs.up || inputs.up2,
         inputs.c_left,
         inputs.c_right,
         inputs.c_down,
