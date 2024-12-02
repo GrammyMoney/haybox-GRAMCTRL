5e1346d184dab715893127977fc0ccf2da44dac8
Bread <breadbandit7@gmail.com>
2024-12-02 12:42:18 -0800

    Update Rivals2.cpp
    Fixed SOCD pair count. Oopsie on my part lol.

--- src/modes/Rivals2.cpp
+++ src/modes/Rivals2.cpp
@@ -8,7 +8,7 @@ bool input_persist; //for angled tilts
 int timer = 0; //for angled tilts
 
 Rivals2::Rivals2(socd::SocdType socd_type) {
-    _socd_pair_count = 4;
+    _socd_pair_count = 5;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
         socd::SocdPair{&InputState::left,    &InputState::right,   socd_type},
         socd::SocdPair{ &InputState::down,   &InputState::up,      socd_type},
